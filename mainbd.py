23303d085ef40bb3dcdc3cc355628f22d87feb7a
varadavishram <varadavishram@gmail.com>
2025-04-02 15:51:32 +0530

    Update mainbd.py

--- mainbd.py
+++ mainbd.py
@@ -1,7 +1,5 @@
 import random
 import numpy as np
-import pickle
-import gzip
 
 class mainc(object):
 
@@ -76,25 +74,4 @@ class mainc(object):
     def sigmoid(self, z):
         return 1.0/(1.0 + np.exp(-z))
 
-#mnist data loading
-
-def load_data():
-    with gzip.open('d:/neural-networks-and-deep-learning-master/data/mnist.pkl.gz', 'rb') as f:
-        return pickle.load(f, encoding="latin1")
-
-def load_data_wrapper():
-    tr_d, va_d, te_d = load_data()  
-    training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
-    training_results = [vec(y) for y in tr_d[1]]
-    training_data = list(zip(training_inputs, training_results))
-    validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
-    validation_data = list(zip(validation_inputs, va_d[1]))
-    test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
-    test_data = list(zip(test_inputs, te_d[1]))
-    return (training_data, validation_data, test_data)
-
-def vec(j):
-    e = np.zeros((10, 1))
-    e[j] = 1.0
-    return e
 
